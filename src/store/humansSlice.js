99bf4f993e97a9e190b84344d2cbd2b8f0c14861
Shawn Su <Shawn@Shawns-MBPR15-2012.local>
2022-06-30 14:06:44 +1200

    add: createHuman factory fn

--- src/store/humansSlice.js
+++ src/store/humansSlice.js
@@ -1,13 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAction, createSlice, nanoid } from '@reduxjs/toolkit';
 
-const initialState = [];
+// factory fn for human object
+const createHuman = (name) => ({
+  id: nanoid(),
+  name,
+  taskIds: []
+});
+
+const initialState = [
+  createHuman('Shawn Su'),
+  createHuman('Steve Kinny'),
+  createHuman('David Church')
+];
 
 const humansSlice = createSlice({
   name: 'humans',
   initialState,
   reducers: {
     addHuman: (state, action) => {
-      state.push(action.payload);
+      state.push(createHuman(action.payload));
     }
   }
 });
