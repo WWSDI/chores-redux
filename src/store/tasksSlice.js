c1b7d12b51aec5c74abda088301d91ade982fb22
Shawn Su <Shawn@Shawns-MBPR15-2012.local>
2022-06-30 13:27:05 +1200

    add: tasks slice

---
+++ src/store/tasksSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const createTask = (title) => ({
+  id: nanoid(),
+  title,
+  completed: false,
+  assignedTo: ''
+});
+
+const initialState = [
+  createTask('order more food ingredient'),
+  createTask('feed the cats')
+];
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(createTask(action.payload));
+    }
+  }
+});
